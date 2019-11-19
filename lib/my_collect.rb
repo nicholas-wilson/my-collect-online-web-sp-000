ca268f83748d213a9503d24537501a9259ce9caf
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 19:06:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  counter = 0
+  new_array = []
 
-
+  while counter < array.size
+    current_element = yield array[counter]
+    new_array << current_element
+    counter += 1
+  end
+  new_array
+end
